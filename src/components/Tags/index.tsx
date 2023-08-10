cb90ef6a5147c99b73f5e2d61d2387c3096fc71d
liwenkai <2020583117@qq.com>
2023-08-10 17:42:06 +0800

    fix: 标签组件样式修改

--- src/components/Tags/index.tsx
+++ src/components/Tags/index.tsx
@@ -1,12 +1,23 @@
+import { useState } from 'react'
+
 const Tags = () => {
+  const tagsItems = ['all', 'CSS', 'java']
+  const [activeIndex, setActiveIndex] = useState(0)
+
   return (
-    <div className="inline-block align-middle my-2">
-      <span className="text-sm  py-1.5 px-3 rounded-full cursor-pointer">
-        All
-      </span>
-      <span className="text-sm bg-pillsBg hover:bg-gray-200 dark:hover:bg-gray-500 py-1 px-3 rounded-full border border-gray-700 transition duration-150 cursor-pointer">
-        CSS
-      </span>
+    <div className="inline-block align-middle my-3">
+      {tagsItems.map((item, index) => {
+        return (
+          <span
+            key={index}
+            className={`text-sm  py-1 px-5 mx-1 rounded-full cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-500 border
+							${activeIndex == index ? 'border-gray-700 transition duration-150' : ''}`}
+            onClick={() => setActiveIndex(index)}
+          >
+            {item}
+          </span>
+        )
+      })}
     </div>
   )
 }
