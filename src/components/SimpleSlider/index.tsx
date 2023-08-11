e9c16fa93782ac73d4540b5d7cb6bea1b1752bfa
liwenkai <2020583117@qq.com>
2023-08-12 00:20:18 +0800

    feat: 轮播图左右导航键修改

--- src/components/SimpleSlider/index.tsx
+++ src/components/SimpleSlider/index.tsx
@@ -1,8 +1,33 @@
+import { FunctionComponent } from 'react'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import { useQuery } from 'react-query'
 import { useBanner } from '@/api/banner'
+import './index.css'
+import SvgIcon from '../SvgIcon'
+
+interface ArrowProps {
+  onClick?: () => void
+}
+
+const CustomPrevArrow: FunctionComponent<ArrowProps> = (props) => {
+  const { onClick } = props
+  return (
+    <div className="custom-prev-arrow" onClick={onClick}>
+      <SvgIcon className=" w-6 h-auto" name="chevron-left" />
+    </div>
+  )
+}
+
+const CustomNextArrow: FunctionComponent<ArrowProps> = (props) => {
+  const { onClick } = props
+  return (
+    <div className="custom-next-arrow" onClick={onClick}>
+      <SvgIcon className=" w-6 h-auto" name="chevron-right" />
+    </div>
+  )
+}
 
 const SimpleSlider = () => {
   const settings = {
@@ -10,7 +35,9 @@ const SimpleSlider = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 4
+    slidesToScroll: 1,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />
   }
   const { isLoading, error, data } = useQuery('useBanner', useBanner)
   console.log(data)
@@ -27,15 +54,14 @@ const SimpleSlider = () => {
     <div className="slick-carousel col-span-full">
       <Slider {...settings}>
         {data.map((item) => (
-          <div
-            key={item.bannerId}
-            className="flex justify-center px-2 outline-0"
-          >
-            <img
-              src={item.pic}
-              alt={item.typeTitle}
-              className="w-full h-auto rounded"
-            />
+          <div key={item.bannerId} className="flex justify-center outline-0">
+            <div className="mx-2 rounded">
+              <img
+                src={item.pic}
+                alt={item.typeTitle}
+                className="w-full h-auto"
+              />
+            </div>
           </div>
         ))}
       </Slider>
