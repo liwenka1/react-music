ddd6cd72daa767330b46e0c62dddca69209a47dd
liwenkai <2020583117@qq.com>
2023-08-15 17:43:39 +0800

    fix: aplayer伪元素背景删除

--- src/components/Aplayer/index.tsx
+++ src/components/Aplayer/index.tsx
@@ -1,6 +1,7 @@
 import 'APlayer/dist/APlayer.min.css'
 import { useEffect } from 'react'
 import useAplayerStore from '@/stores/aplayer'
+import './index.css'
 
 const Aplayer = () => {
   const { ap, initializeAplayer } = useAplayerStore()
@@ -14,7 +15,7 @@ const Aplayer = () => {
   }, [])
 
   return (
-    <div className="playMusic before:bg-transparent after:bg-transparent w-full lg:w-4/5 xl:w-5/6 fixed bottom-0 z-20 bg-background p-0 m-0"></div>
+    <div className="playMusic w-full lg:w-4/5 xl:w-5/6 fixed bottom-0 z-20 bg-background p-0 m-0"></div>
   )
 }
 
