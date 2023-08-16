b5d7bc9b7ab5d28ccfe27f3b24bbd56a193ca6cb
liwenkai <2020583117@qq.com>
2023-08-16 21:42:08 +0800

    fix: 简单修改布局为左右分布

--- src/components/Aplayer/index.tsx
+++ src/components/Aplayer/index.tsx
@@ -14,9 +14,7 @@ const Aplayer = () => {
     }
   }, [])
 
-  return (
-    <div className="playMusic w-full lg:w-4/5 xl:w-5/6 fixed bottom-0 z-20 bg-background p-0 m-0"></div>
-  )
+  return <div className="playMusic w-full h-auto bg-background p-0 m-0"></div>
 }
 
 export default Aplayer
