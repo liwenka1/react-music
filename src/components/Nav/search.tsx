a93c96eda11e00ded9785fd7d8324b0d405750b1
liwenkai <2020583117@qq.com>
2023-08-17 22:40:40 +0800

    fix: 使用预配置的文本颜色

--- src/components/Nav/search.tsx
+++ src/components/Nav/search.tsx
@@ -5,12 +5,12 @@ const Search = () => {
     <div className="hidden md:flex items-center space-x-2 w-1/2">
       <div className="flex items-center flex-auto justify-between rounded border border-gray-500">
         <input
-          className="w-full rounded-l outline-none border-none p-0.5 pl-3 placeholder-gray-500 dark:text-white bg-background"
+          className="w-full rounded-l outline-none border-none p-0.5 pl-3 placeholder-gray-500 text-primary bg-background"
           type="text"
           placeholder="Search"
         />
         <SvgIcon
-          className="md-21 text-center text-gray-300 hover:text-white p-1 w-20 h-7 bg-gray-500 cursor-pointer"
+          className="md-21 text-center text-primary p-1 w-20 h-7 bg-gray-500 cursor-pointer"
           name="magnifying-glass"
         />
       </div>
