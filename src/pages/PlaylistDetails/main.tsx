7a0998ac4abfdec0305fbc9b65f86ce763e59ee0
liwenkai <2020583117@qq.com>
2023-08-23 23:21:34 +0800

    fix: 歌单详情页样式基本完成

--- src/pages/PlaylistDetails/main.tsx
+++ src/pages/PlaylistDetails/main.tsx
@@ -27,7 +27,7 @@ const Main: React.FC<Props> = (props) => {
         </span>
       </div>
       <div>
-        <div className="grid grid-cols-12 gap-4 my-4 text-xs font-light">
+        <div className="grid grid-cols-12 gap-4 mt-8 mb-1 text-xs font-light">
           <span className="col-span-5">歌曲</span>
           <span className="col-span-3">歌手</span>
           <span className="col-span-3">专辑</span>
