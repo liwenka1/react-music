1954ecf4855d975c6bfa9eea2153c1df9440d6bf
liwenkai <2020583117@qq.com>
2023-09-11 16:19:49 +0800

    fix: 修复测试

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import viteEslint from 'vite-plugin-eslint'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import path from 'path'
 
+function AplayerResolve() {
+  return {
+    libraryName: '@APlayer/APlayer',
+    libraryNameChangeCase: 'pascalCase',
+    resolveStyle: () => {
+      return `@APlayer/APlayer/dist/APlayer.min.css`
+    }
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -12,16 +22,12 @@ export default defineConfig({
     createSvgIconsPlugin({
       iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
       symbolId: 'icon-[dir]-[name]'
-    })
+    }),
+    AplayerResolve()
   ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
-    }
-  },
-  build: {
-    rollupOptions: {
-      external: ['APlayer']
     }
   }
 })
